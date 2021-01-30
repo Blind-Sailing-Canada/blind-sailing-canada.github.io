55035a72c0f583f1a1d1579f61abb1bc74467658
lex <emailforalexgo@gmail.com>
2021-01-29 22:17:41 -0500

    v2

---
+++ js/up-down-scroller.js
@@ -0,0 +1,38 @@
+
+function setUpDownScroller (upDownScrollerId) {
+    const upDownScroller = document.getElementById(upDownScrollerId);
+    const upScroller = document.getElementById('up-scroller');
+    const downScroller = document.getElementById('down-scroller');
+
+    const scrollListener = () => {
+        upDownScroller.style.top = window.scrollY + window.innerHeight - 70;
+
+        if (window.pageYOffset > 400) {
+            upScroller.classList.remove("hide");
+        } else {
+            upScroller.classList.add("hide");
+        }
+
+
+        if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 5) {
+            downScroller.classList.add("hide");
+        } else {
+            downScroller.classList.remove("hide");
+        }
+    }
+
+    upScroller.onclick = function () {
+        window.scrollBy(0, -window.innerHeight);
+        return false;
+    };
+
+    downScroller.onclick = function () {
+        console.log("window.innerHeight",window.innerHeight);
+        window.scrollBy(0, window.innerHeight);
+        return false;
+    };
+
+    scrollListener()
+
+    window.addEventListener('scroll', scrollListener);
+}
